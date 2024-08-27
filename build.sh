9a7bfa962c08ce9847e51bc8bc46982e97744df2
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 11:44:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ Image_Tag="latest"
 #checking the directory is present if not the script fails.
 if [ ! -d "$DIRECTORY" ]; then
   echo "$DIRECTORY does not exist."
+  echo "exiting"
   exit 1
 fi
 
