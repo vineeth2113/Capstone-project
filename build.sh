2cd3a3ca84a82e4378c9159c5d146c904833b34a
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 11:45:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 #Buiding the docker image
+sudo su
 docker build -t ${username}/${repo}:${Image_Tag} .
 
 echo "Image ${username}/${repo}:${Image_Tag} is build successfully."
