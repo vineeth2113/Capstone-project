fd467351390752e994f6356a7bebe8e051a9ab14
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 17:59:03 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 #DOCKER_USERNAME="vineeth2108"
 repo="capstone-dev"
+prodrepo="capstone-production"
 tag="latest"
 
 if [[ $GIT_BRANCH == "dev" ]]; then
@@ -20,5 +21,6 @@ elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh './build.sh'
     echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
     docker images
-    docker push vineeth2108/capstone-production:latest
+    docker tag vineeth2108 vineeth2108/${prodrepo}:${tag}
+    docker push vineeth2108/${prodrepo}:${tag}
 fi
