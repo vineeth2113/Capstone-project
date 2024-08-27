1410f3cb60b6c0b4674a05243d0557bff44050c0
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 17:44:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ APPLICATION_NAME="react-app"
 DIRECTORY="build"
 username="vineeth2108"
 repo="capstone-dev"
-prodrepo="capstone-production"
 tag="latest"
 
 echo "checking and building the image"
@@ -16,16 +15,8 @@ if [ ! -d "$DIRECTORY" ]; then
   echo "exiting"
   exit 1
 fi
-
-if [[ $GIT_BRANCH == "dev" ]]; then
   #Buiding the docker image
   docker build -t ${username}/${repo}:${tag} .
 
   echo "Dev Image ${username}/${repo}:${tag} is build successfully."
-
-elif [[ $GIT_BRANCH == "main" ]]; then
-  #Buiding the docker image
-  docker build -t ${username}/${prodrepo}:${tag} .
-  echo "Prod Image ${username}/${prodrepo}:${tag} is build successfully."
-fi
 
