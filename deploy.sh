d72480342e2e3c5d98e38bca64ca837b022b49d1
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 18:43:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ if [[ $GIT_BRANCH == "dev" ]]; then
     docker push vineeth2108/${repo}:${tag}
     
 elif [[ $GIT_BRANCH == "main" ]]; then
+    echo "you are in main branch"
     chmod +x build.sh
     sh './build.sh'
     echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
@@ -24,5 +25,7 @@ elif [[ $GIT_BRANCH == "main" ]]; then
     docker tag vineeth2108 vineeth2108/${prodrepo}:${tag}
     docker push vineeth2108/${prodrepo}:${tag}
 fi
-
+else
+    echo "Failed"
+fi
 
