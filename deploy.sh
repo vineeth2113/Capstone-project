89bb03f2bec667bc79a205a5e84baa13a0c41cf7
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 18:44:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,8 @@ elif [[ $GIT_BRANCH == "main" ]]; then
     echo "you are in main branch"
     chmod +x build.sh
     sh './build.sh'
-    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+    echo "Logging into Docker Hub..."
+    echo "$Dockerhub_password" | docker login --username "$Dockerhub_Username" --password-stdin
     docker images
     docker tag vineeth2108 vineeth2108/${prodrepo}:${tag}
     docker push vineeth2108/${prodrepo}:${tag}
