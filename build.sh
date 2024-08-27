50bdcfe7bd63904a80b4294aba7e529a81c81052
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-08-27 09:17:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+#creating the name if the image.
+IMAGE_NAME="Vineeth/React-app"
+DIRECTOR = "build"
+
+Image_Tag = `date +%Y.%m.%d.%H.%M`
+
+#checking the directory is present if not the script fails.
+if [ ! -d "$DIRECTORY" ]; then
+  echo "$DIRECTORY does not exist."
+  exit 1
+fi
+
+#Buiding the docker image
+docker build -t ${IMAGE_NAME}:${Image_Tag}
+
+
+echo "Image ${IMAGE_NAME}:${Image_Tag} is build successfully."
+
 
